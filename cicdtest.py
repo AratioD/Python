ba8cb3b788030698b6800d7705051ccbf0c64dfa
AratioD <fffb@ddd.com>
2020-04-10 16:34:39 +0300

    first line of code

--- cicdtest.py
+++ cicdtest.py
@@ -3,4 +3,14 @@
 names_hobbies = {'jack': 'camping', 'minna': 'scout', 'Rachel': 'theater'}
 names_proffessions = {'minna': 'manager', 'jack': 'coder', 'Rachel': 'theacher'}
 
+# print(names_hobbies)
+# print(names_proffessions)
 
+
+def printlist(list):
+    for item in list.items():
+        print(item)
+
+
+printlist(names_hobbies)
+printlist(names_proffessions)
