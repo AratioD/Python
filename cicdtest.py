dc2b782b244cc3568fb111dcec7acaedb94125e7
AratioD <fffb@ddd.com>
2020-04-10 18:55:22 +0300

    copied a full example python code

--- cicdtest.py
+++ cicdtest.py
@@ -1,16 +1,27 @@
+from __future__ import print_function
+import random
 
-# dictionary with mixed keys
-names_hobbies = {'jack': 'camping', 'minna': 'scout', 'Rachel': 'theater'}
-names_proffessions = {'minna': 'manager', 'jack': 'coder', 'Rachel': 'theacher'}
-
-# print(names_hobbies)
-# print(names_proffessions)
-
-
-def printlist(list):
-    for item in list.items():
-        print(item)
+buzz = ('continuous testing', 'continuous integration',
+        'continuous deployment', 'continuous improvement', 'devops', "jipykayyy", "hurrayyyyyyy")
+adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
+           'seriously')
+verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
 
-printlist(names_hobbies)
-printlist(names_proffessions)
+def sample(l, n=1):
+    result = random.sample(l, n)
+    if n == 1:
+        return result[0]
+    return result
+
+
+def generate_buzz():
+    buzz_terms = sample(buzz, 2)
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
+                       sample(verbs), buzz_terms[1]])
+    return phrase.title()
+
+
+if __name__ == "__main__":
+    print(generate_buzz())
