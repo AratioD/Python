f7a1d88595c72cecb2fbb8e6d750753c54015349
AratioD <fffb@ddd.com>
2020-04-10 16:32:25 +0300

    first line of code

---
+++ cicdtest.py
@@ -0,0 +1,6 @@
+
+# dictionary with mixed keys
+names_hobbies = {'jack': 'camping', 'minna': 'scout', 'Rachel': 'theater'}
+names_proffessions = {'minna': 'manager', 'jack': 'coder', 'Rachel': 'theacher'}
+
+
